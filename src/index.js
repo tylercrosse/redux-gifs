525244af26f2864f058423ed547ea6c9a8f25d94
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-11-02 16:42:28 -0700

    Add react router with boilerplate components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React          from 'react';
 import ReactDOM       from 'react-dom';
 import { Provider }   from 'react-redux';
-import App            from './containers/App.jsx'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import Home           from './containers/Home.jsx'
+import App            from './components/App.jsx'
+import Signup         from './components/Signup.jsx'
+import Login          from './components/Login.jsx'
+import Favorites      from './components/Favorites.jsx'
 import configureStore from './store/configureStore'
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home} />
+        <Route path='signup' component={Signup} />
+        <Route path='login' component={Login} />
+        <Route path='favorites' component={Favorites} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
